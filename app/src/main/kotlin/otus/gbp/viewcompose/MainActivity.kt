04f5f4dda01f800e4eedfa065c191c39f3e7eb05
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-09-08 12:04:21 +0200

    Boilerplate

---
+++ app/src/main/kotlin/otus/gbp/viewcompose/MainActivity.kt
@@ -0,0 +1,18 @@
+package otus.gbp.viewcompose
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import otus.gbp.viewcompose.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
